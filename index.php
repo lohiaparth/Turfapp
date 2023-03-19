8583fb7ea51c803961345591671da1283aa0dffe
Parth Lohia <parth.r.lohia@gmail.com>
2023-03-20 00:47:24 +0530

    modified buttons and created slider in index.php

--- index.php
+++ index.php
@@ -3,20 +3,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Siddhi/HomeStyles.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <h3>about us</h3>
-        <h3>contact us</h3>
-        <h3>help</h3>
-        <h3>find match</h3>
-    </header>
+        <header>
+            <a class="logo" href="/"><img src="images/logo.svg" alt="logo"></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="#">Log out</a>
+        </header>
 
-    <button onclick="window.location.href='secondarypages/invite.php'">Invite Players</button>
-    <button onclick="window.location.href='secondarypages/viewteam.php'">View Team</button>
-    <button onclick="window.location.href='secondarypages/findmatch.php'">Find Match</button>
+        <script src="slider-script.js"></script>
 
-    
+        <div class="row">
+            <div class="auto-slider">
+                <div id="img" class="fade-in">
+                    <img  src="football-slider.jpeg"/>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="row">
+            <div class="page-buttons">
+
+                <button  onclick="window.location.href='secondarypages/invite.php'">Invite Players</button>
+                <button  onclick="window.location.href='secondarypages/viewteam.php'">View Team</button>
+                <button  onclick="window.location.href='secondarypages/findmatch.php'">Find Match</button>
+            </div>
+
+        </div>
 </body>
 </html>
