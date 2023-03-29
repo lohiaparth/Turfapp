ce1de85c1f31dae1e01643b46fd1872284d1b468
Parth Lohia <parth.r.lohia@gmail.com>
2023-03-30 00:03:24 +0530

    created extra jslogin.php for session id

---
+++ secondarypages/jslogin.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once('config.php');
+
+
+$loginemail = $_POST['loginemail'];
+$loginpassword = $_POST['loginpassword'];
+
+$sql = "SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1"; //try db names of fields if doesnt work
+$stmtselect  = $db->prepare($sql);
+$result = $stmtselect->execute([$loginemail, $loginpassword]);
+
+if($result){
+	$user = $stmtselect->fetch(PDO::FETCH_ASSOC);
+	if($stmtselect->rowCount() > 0){
+		$_SESSION['userlogin'] = $user;
+		$_SESSION['user_email'] = $user['email'];
+		// echo $_SESSION['user_email'];
+		echo '1';
+	}else{
+		echo 'There no user for that combo';		
+	}
+}else{
+	echo 'There were errors while connecting to database.';
+}
