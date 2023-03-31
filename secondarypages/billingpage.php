f0d68edf035ba861f67df5c9d6c1f6ac0c5bbd6a
Siddhi <siddhidmahajan@gmail.com>
2023-03-31 12:07:22 +0530

    billing and payment page

--- secondarypages/billingpage.php
+++ secondarypages/billingpage.php
@@ -1,12 +1,30 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="billing.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Billing</h1>
-    
+    <div class="sort-row1">
+        <h1 class="h1-invite">BILLING</h1>
+        <h5 class="h5-invite">Please pay your bill</h5>
+        <div class="wrap">
+            <div class="view-text">
+                <div class="view-fri">
+                    <div>
+                  <h2 class="view-name">siddhidmahajan@gmail.com</h2>
+                </div>
+                <div>
+                  <h6 class="view-numb">₹200</h6>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
