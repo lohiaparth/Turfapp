426a1cd2d7e252a9f26f6ba037b0103cda2509f9
Siddhi <siddhidmahajan@gmail.com>
2023-03-31 11:18:14 +0530

    Added price

--- secondarypages/findmatch.php
+++ secondarypages/findmatch.php
@@ -86,6 +86,10 @@ require_once("config.php");
                 <!-- turf sports -->
                 <h4 class="h4-invite">basketball & football</h4>
             </div>
+            <div class="turf-sports-money">
+                <!-- turf sports -->
+                <h4 class="h3-invite" style="font-weight:bold;">₹2000</h4>
+            </div>
             <div class="turf-sports">
                 <!-- selection button -->
                 <button class="selection">Select</button>
